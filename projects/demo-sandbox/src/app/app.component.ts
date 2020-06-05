f3ebe44ef9c19ab4149ac7f555aaa8b90e12694b
Mateh Elismar <matematicoelismar@gmail.com>
2020-06-05 11:00:04 -0400

    Validador de cedulas agregado

--- projects/demo-sandbox/src/app/app.component.ts
+++ projects/demo-sandbox/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from "@angular/core";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
+import { FormComponentBase } from "projects/ng-m-validation/src/public-api";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'demo-sandbox';
+export class AppComponent extends FormComponentBase implements AfterViewInit {
+  title = "demo-sandbox";
+
+  form: FormGroup;
+
+  validationMessages = {
+    name: { required: "Name is required" },
+    email: {
+      required: "email is required",
+      email: "email is not well-formated",
+    },
+    cedula: { required: "Cedula is required", cedula: "Invalid cedula" },
+  };
+
+  formErrors = {};
+
+  constructor(fb: FormBuilder) {
+    super();
+    this.form = fb.group({
+      name: ["", [Validators.required]],
+      email: ["", [Validators.required, Validators.email]],
+      cedula: ["", [Validators.required, this.cedulaValidator]],
+    });
+  }
+
+  ngAfterViewInit() {
+    this.startControlMonitoring(this.form);
+  }
 }
--- projects/ng-m-validation/src/lib/form-component-base.ts
+++ projects/ng-m-validation/src/lib/form-component-base.ts
@@ -5,6 +5,7 @@ import {
   FormArray,
   ValidatorFn,
   ValidationErrors,
+  FormControl,
 } from "@angular/forms";
 import { Observable, fromEvent, merge, Subscriber, Subscription } from "rxjs";
 import { debounceTime } from "rxjs/operators";
@@ -13,7 +14,9 @@ export class FormComponentBase implements OnDestroy {
   @ViewChildren(FormControlName, { read: ElementRef })
   formInputElements: QueryList<ElementRef>;
 
-  public validationMessages: { [key: string]: { [key: string]: string } } = {};
+  public validationMessages: {
+    [key: string]: { [key: string]: string };
+  } = {};
   public formErrors: { [key: string]: string } = {};
 
   mySubs: Subscription;
@@ -117,6 +120,94 @@ export class FormComponentBase implements OnDestroy {
       }
       return null;
     };
+  }
+
+  cedulaValidator(formControl: FormControl) {
+    const cedula = formControl.value;
+    if (cedula) {
+      let rootCedula = cedula.trim();
+
+      let str1 = rootCedula.substr(0, 3);
+      let str2 = rootCedula.substr(4, 7);
+      let str3 = rootCedula.substr(12, 1);
+      let fixedCedula = str1.concat(str2, str3);
+
+      let numeroCed = fixedCedula;
+
+      for (let f = 0; f < numeroCed.length; f++) {}
+
+      let cPrimerNumero = numeroCed[0] * 1;
+      let cSegundoNumero = numeroCed[1] * 2;
+      let cTercerNumero = numeroCed[2] * 1;
+      let cCuartoNumero = numeroCed[3] * 2;
+      let cQuintoNumero = numeroCed[4] * 1;
+      let cSextoNumero = numeroCed[5] * 2;
+      let cSeptimoNumero = numeroCed[6] * 1;
+      let cOctavoNumero = numeroCed[7] * 2;
+      let cNovenoNumero = numeroCed[8] * 1;
+      let cDecimoNumero = numeroCed[9] * 2;
+      let digitoVerificador = parseInt(numeroCed[10]);
+
+      if (cPrimerNumero > 9) {
+        cPrimerNumero = cPrimerNumero - 9;
+      }
+      if (cSegundoNumero > 9) {
+        cSegundoNumero = cSegundoNumero - 9;
+      }
+      if (cTercerNumero > 9) {
+        cTercerNumero = cTercerNumero - 9;
+      }
+      if (cCuartoNumero > 9) {
+        cCuartoNumero = cCuartoNumero - 9;
+      }
+      if (cQuintoNumero > 9) {
+        cQuintoNumero = cQuintoNumero - 9;
+      }
+      if (cSextoNumero > 9) {
+        cSextoNumero = cSextoNumero - 9;
+      }
+      if (cSeptimoNumero > 9) {
+        cSeptimoNumero = cSeptimoNumero - 9;
+      }
+      if (cOctavoNumero > 9) {
+        cOctavoNumero = cOctavoNumero - 9;
+      }
+      if (cNovenoNumero > 9) {
+        cNovenoNumero = cNovenoNumero - 9;
+      }
+      if (cDecimoNumero > 9) {
+        cDecimoNumero = cDecimoNumero - 9;
+      }
+
+      let sumaPares =
+        cSegundoNumero +
+        cCuartoNumero +
+        cSextoNumero +
+        cOctavoNumero +
+        cDecimoNumero;
+      let sumaImpares =
+        cPrimerNumero +
+        cTercerNumero +
+        cQuintoNumero +
+        cSeptimoNumero +
+        cNovenoNumero;
+      let sumaParesImpares = sumaPares + sumaImpares;
+      let sumaDecena = Math.ceil(sumaParesImpares) * 10;
+      sumaParesImpares = sumaDecena - sumaParesImpares;
+
+      let cadenaSumaParesImpares = sumaParesImpares.toString();
+
+      for (let j = 0; j < cadenaSumaParesImpares.length; j++) {}
+
+      let g = cadenaSumaParesImpares.trim();
+      let w = cadenaSumaParesImpares[2];
+
+      if (parseInt(w) == digitoVerificador) {
+        return null;
+      } else {
+        return { cedula: true };
+      }
+    }
   }
 
   ngOnDestroy() {
